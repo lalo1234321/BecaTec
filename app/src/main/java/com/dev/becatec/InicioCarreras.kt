2508c46f8caae8583e0d4bd1c769c8a5817dfa87
taniawlaf <taniapao51197@outlook.com>
2022-10-03 21:09:15 -0500

    segundo spinner

--- app/src/main/java/com/dev/becatec/InicioCarreras.kt
+++ app/src/main/java/com/dev/becatec/InicioCarreras.kt
@@ -1,23 +1,25 @@
 package com.dev.becatec
 
-import android.app.Activity
 import android.os.Bundle
 import android.view.View
-import android.view.inputmethod.InputBinding
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.Spinner
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 
-public abstract class InicioCarreras:AppCompatActivity(), AdapterView.OnItemSelectedListener{
+class InicioCarreras:AppCompatActivity(), AdapterView.OnItemSelectedListener{
     lateinit var spinner: Spinner
     lateinit var textView: TextView
+    lateinit var spinner1: Spinner
+    lateinit var textView1: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.pagina_inicio)
         textView = findViewById(R.id.textView)
-        spinner = findViewById(R.id.spinner)
+        spinner = findViewById(R.id.spinner2)
+        textView1 = findViewById(R.id.textView)
+        spinner1 = findViewById(R.id.spinner3)
         val adapter = ArrayAdapter.createFromResource(
             this,
             R.array.Carreras,
@@ -26,7 +28,14 @@ public abstract class InicioCarreras:AppCompatActivity(), AdapterView.OnItemSele
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
         spinner.adapter = adapter
         spinner.onItemSelectedListener = this
-
+        val adaptor = ArrayAdapter.createFromResource(
+            this,
+            R.array.Becas,
+            android.R.layout.simple_spinner_item
+        )
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+        spinner1.adapter = adaptor
+        spinner1.onItemSelectedListener = this
     }
 
     override fun onNothingSelected(parent: AdapterView<*>?) {
